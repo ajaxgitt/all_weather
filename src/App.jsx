24e0b592d08299ca164bb80af3ee573b72102de5
ajaxgitt <164401823+ajaxgitt@users.noreply.github.com>
2024-12-03 21:05:44 -0400

    clima animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="home">
+      <div className="content">
+        <div>
+          <input type="text" placeholder="please write the name of the city" />
+          <i className=" lupa fa-solid fa-magnifying-glass" />
+        </div>
+        <div className="results">
+          <h2> Santa Cruz</h2>
+          <p>38°</p>
+          <span  >Mayormente despejado</span>
+          <p>H: 18° L: 13°</p>
+        </div>
+        <div className="details">
+          <div className="temperatura">
+            <i className="fa-solid fa-temperature-low" />
+            <div className="det">
+              <p>Sensacion Termica</p>
+              <p>14°</p>
+            </div>
+          </div>
+
+          <div className="gota">
+            <i className="fa-solid fa-droplet" />
+            <div className="det">
+              <p>Humedad</p>
+              <p>15%</p>
+            </div>
+          </div>
+          <div className="nube">
+            <i className="fa-solid fa-cloud" />
+            <dev className="det">
+              <p>Nubes</p>
+              <p>20%</p>
+            </dev>
+          </div>
+          <div className="direccion">
+            <i className="fa-solid fa-arrow-right" />
+            <div className="det">
+              <p>Direccion del viento</p>
+              <p>de norte a sur</p>
+            </div>
+          </div>
+          <div className="viento">
+            <i className="fa-solid fa-wind" />
+            <div className="det">
+              <p>Velocidad de viento</p>
+              <p>3mph</p>
+            </div>
+          </div>
+          <div className="presion">
+            <i className="fa-solid fa-chart-line" />
+            <div className="det">
+              <p>Presion</p>
+              <p>1015mb</p>
+            </div>
+          </div>
+          <div className="ojo">
+            <i className="fa-regular fa-eye" />
+            <div className="det">
+              <p>Vision</p>
+              <p>5 km</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
